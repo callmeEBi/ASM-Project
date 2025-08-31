7512ebf50ee96869d44cefa4e4ebca1fc6ce1d62
callmeEBi <152092732+callmeEBi@users.noreply.github.com>
2025-08-31 17:31:10 +0330

    fixed some bugs
    - fixed recognizing zero and plus sign (+).

--- proj.asm
+++ proj.asm
@@ -1,18 +1,17 @@
-.DATA
-    input_buffer    DB 80, 0, 81 DUP(?)
-    
-    prompt_msg      DB 'Enter input (max 80 chars): $'
-    number_pos_msg  DB 0Dh, 0Ah, 'Result: Positive number$'
-    number_neg_msg  DB 0Dh, 0Ah, 'Result: Negative number$'
-    string_part1_msg DB 0Dh, 0Ah, 'Result: String with $'
-    string_part2_msg DB ' word(s)$'
-    empty_msg       DB 0Dh, 0Ah, 'Error: Empty input$'
-    
-    num_buffer      DB 6 DUP('$')
-    
-    word_count      DW 0
-    is_number       DB 1
-    is_negative     DB 0
+.DATA       
+
+input_buffer DB 80, 0, 81 DUP(?)
+prompt_msg DB 'Enter input (max 80 chars): $'
+number_pos_msg DB 0DH, 0AH, 'Result: Positive number$'
+number_neg_msg DB 0DH, 0AH, 'Result: Negative number$'
+number_zero_msg DB 0DH, 0AH, 'Result: ZERO$'
+string_part1_msg DB 0DH, 0AH, 'Result: String with $'
+string_part2_msg DB ' word(s)$'
+empty_msg DB 0DH, 0AH, 'Error: Empty input$'
+num_buffer DB 6 DUP('$')
+word_count DW 0
+is_number DB 1
+is_negative DB 0
 
 .CODE
 MAIN PROC
@@ -20,12 +19,12 @@ MAIN PROC
     MOV DS, AX
     
     LEA DX, prompt_msg
-    MOV AH, 09h
-    INT 21h
+    MOV AH, 09H
+    INT 21H
     
     LEA DX, input_buffer
-    MOV AH, 0Ah
-    INT 21h
+    MOV AH, 0AH
+    INT 21H
     
     LEA SI, input_buffer[2]
     MOV CL, input_buffer[1]
@@ -38,14 +37,25 @@ MAIN PROC
     
     MOV AL, [SI]
     CMP AL, '-'
-    JNE analysis_loop
+    JNE check_for_plus
     
     MOV is_negative, 1
     INC SI
     DEC CX
     CMP CX, 0
     JE not_a_number
+    JMP analysis_loop                                                   
 
+check_for_plus:
+    CMP AL, '+'
+    JNE analysis_loop
+    
+    MOV is_negative, 0
+    INC SI
+    DEC CX
+    CMP CX, 0
+    JE not_a_number
+    
 analysis_loop:
     CMP CX, 0
     JE end_analysis
@@ -83,35 +93,41 @@ end_analysis:
     JE process_as_number
     
     LEA DX, string_part1_msg
-    MOV AH, 09h
-    INT 21h
+    MOV AH, 09H
+    INT 21H
     
     CALL display_word_count
     
     LEA DX, string_part2_msg
-    MOV AH, 09h
-    INT 21h
+    MOV AH, 09H
+    INT 21H
     JMP exit_program
 
 process_as_number:
     CMP is_negative, 1
-    JE show_negative
+    JE show_negative  
+    CMP input_buffer[2], 30H
+    JE show_zero
     LEA DX, number_pos_msg
     JMP show_result
 show_negative:
     LEA DX, number_neg_msg
+    JMP show_result
+    
+show_zero:
+    LEA DX, number_zero_msg
     JMP show_result
 
 show_empty:
     LEA DX, empty_msg
 
 show_result:
-    MOV AH, 09h
-    INT 21h
+    MOV AH, 09H
+    INT 21H
 
 exit_program:
-    MOV AH, 4Ch
-    INT 21h
+    MOV AH, 4CH
+    INT 21H
 
 MAIN ENDP
 
@@ -137,10 +153,9 @@ conversion_loop:
 print_number_buffer:
     INC DI
     LEA DX, DI
-    MOV AH, 09h
-    INT 21h
+    MOV AH, 09H
+    INT 21H
     RET
 display_word_count ENDP
 
 END MAIN
-
